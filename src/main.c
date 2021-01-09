ad22a009b924f67219e42ffbaa7438c267c8bd00
Patrick Erdmann <pe@perdmann.de>
2021-01-09 17:28:23 +0100

    Handle two file descriptors with epoll. Add the ability to toggle the timer by pressing enter.

--- src/main.c
+++ src/main.c
@@ -39,6 +39,8 @@ int msg_id;
 int epoll_fd;
 mqd_t mq;
 const char* queue_name = "/ExampleMQ";
+int max_fd;
+timer_t _timer;
 
 void timer_signal(union sigval val) {
 	int ret;
@@ -53,7 +55,7 @@ void timer_signal(union sigval val) {
 
 	if (mq < 0) {
 		perror("mq_open failed");
-	}	
+	}
 
 	for (int i = 0; i <= random; i++) {
 		sprintf(buf_tmp, "NUM: %d %s", i, buf);
@@ -70,7 +72,6 @@ void timer_signal(union sigval val) {
 }
 
 void _init_timer() {
-	timer_t _timer;
 
 	pthread_attr_t attr;
 	pthread_attr_init( &attr );
@@ -90,9 +91,9 @@ void _init_timer() {
 	if (ret == 0) {
 		struct itimerspec in, out;
 		in.it_value.tv_sec = 0;
-		in.it_value.tv_nsec = 1;
-		in.it_interval.tv_sec = 1;
-		in.it_interval.tv_nsec = 10000000; // 10 ms
+		in.it_value.tv_nsec = 0;
+		in.it_interval.tv_sec = 0;
+		in.it_interval.tv_nsec =  0; //10000000; // 10 ms
 
 		ret = timer_settime(_timer, 0, &in, &out);
 		if(ret < 0) {
@@ -101,7 +102,7 @@ void _init_timer() {
 	}
 }
 
-void _init_msg_queue() {
+bool _init_msg_queue() {
 	struct mq_attr attr;
 	struct sigevent sev;
 
@@ -110,21 +111,57 @@ void _init_msg_queue() {
 	attr.mq_msgsize = READ_BYTES;
 	attr.mq_curmsgs = 0;
 
-	mq = mq_open(queue_name, O_CREAT | O_RDWR, 0644, &attr);
+    mq = mq_open(queue_name, O_CREAT | O_RDWR, 0666, &attr);
 
 	if (mq < 0) {
 		perror("init mq_open failed");
+        return false;
 	}
+    return true;
 }
 
 void _init_epoll() {
-	struct epoll_event event;
+	struct epoll_event event_stdin;
+	struct epoll_event event_mq;
 
 	epoll_fd = epoll_create1(0);
-	event.events = EPOLLIN;
 
-	event.data.fd = mq;
-	epoll_ctl(epoll_fd, EPOLL_CTL_ADD, mq, &event);
+	event_stdin.events = EPOLLIN;
+	event_mq.events = EPOLLIN;
+
+    event_stdin.data.fd =  0;
+	event_mq.data.fd = mq;
+
+    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, 0, &event_stdin);
+	epoll_ctl(epoll_fd, EPOLL_CTL_ADD, mq, &event_mq);
+}
+
+bool toggle_timer() {
+    static bool enabled = false;
+    struct itimerspec old, new;
+
+	new.it_value.tv_sec = 0;
+
+    timer_gettime(_timer, &old);
+    printf("Current timer: %d.%d \n", old.it_interval.tv_sec, old.it_interval.tv_nsec);
+
+    if (old.it_interval.tv_sec == 0 && old.it_interval.tv_nsec == 0) {
+		new.it_interval.tv_nsec =  100 * 1000 * 1000; // 100 ms
+	    new.it_value.tv_nsec = 1;
+	    new.it_interval.tv_sec = 1;
+        enabled = true;
+    } else {
+		new.it_interval.tv_nsec =  0;
+	    new.it_value.tv_nsec = 0;
+	    new.it_interval.tv_sec = 0;
+        enabled = false;
+    }
+
+    int err = timer_settime(_timer, 0, &new, &old);
+    if (err < 0) {
+        perror("Settime failed");
+    }
+    return enabled;
 }
 
 int main() {
@@ -135,7 +172,9 @@ int main() {
 	char read_buffer[READ_BYTES];
 	size_t bytes_read;
 
-	_init_msg_queue();
+	if (!_init_msg_queue()) {
+        return 1;
+    }
 	_init_epoll();
 	_init_timer();
 
@@ -143,10 +182,26 @@ int main() {
 		event_count = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
 		printf("%d ready events\n", event_count);
 		for(int event_index = 0; event_index < event_count; event_index++) {
-			bytes_read = mq_receive(mq, read_buffer, READ_BYTES, 0);
-			printf("%zd bytes read.\n", bytes_read);
-			read_buffer[bytes_read] = '\0';
-			printf("Read '%s'\n", read_buffer);
+			if (events[event_index].data.fd == mq) { // this is a messagequeue
+                bytes_read = mq_receive(events[event_index].data.fd, read_buffer, READ_BYTES, 0);
+                if (bytes_read > 0) {
+			        printf("%zd bytes read %d mq_fd: %d\n", bytes_read, events[event_index].data.fd, mq);
+                    read_buffer[bytes_read] = '\0';
+			        printf("Read '%s'\n", read_buffer);
+                } else {
+                    perror("read failed");
+                }
+            } else { // normal STDIN FD
+                read(0, read_buffer, 1);
+                if (read_buffer[0] == 0x0a) { // handle Enter
+                    // toggle Timer
+                    if (toggle_timer()) {
+                        printf("Enable Timer \n");
+                    } else {
+                        printf("Enable Timer \n");
+                    }
+                }
+            }
 		}
 	}
 }
